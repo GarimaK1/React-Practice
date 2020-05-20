0a7638199d98da44ce9afcf20c8c1333c36755e8
Garima <garima.k.03@gmail.com>
2020-05-19 22:20:24 -0400

    deleteLog working with redux and backend

--- 4_it_logger_redux/src/components/logs/LogItem.js
+++ 4_it_logger_redux/src/components/logs/LogItem.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import { deleteLog } from '../../actions/logActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog }) => {
+    const handleDelete = () => {
+        deleteLog(log.id);
+        M.toast({ html: 'Log deleted.'});
+    }
     return (
         <li className="collection-item">
             <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>
                 {log.message}
             </a>
-            <a href="#!" className="secondary-content" >
+            <a href="#!" className="secondary-content" onClick={handleDelete}>
                 <i className="material-icons grey-text">delete</i>
             </a>
             <p className="grey-text">
@@ -23,7 +30,9 @@ const LogItem = ({ log }) => {
 }
 
 LogItem.propType = {
-    log: PropTypes.object.isRequired
+    log: PropTypes.object.isRequired,
+    deleteLog: PropTypes.func.isRequired
 }
 
-export default LogItem;
+// not bringing any state as prop to component. So putting null in place of 'mapStateToProps'
+export default connect(null, { deleteLog })(LogItem);
